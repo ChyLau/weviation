9d7eb9110a150d0cf812ffaabbe7a6bbb48245db
Chy Lau <chyho.lau@gmai.com>
2014-11-18 23:57:22 +0100

    XML file now includes the unit and type. This format modifies the parser, which now finds the desired elements.

--- weviation/parse.py
+++ weviation/parse.py
@@ -15,18 +15,24 @@ def parse_xml():
     data3 = {}
 
     for method in fp.findall('torenbeek'):
-        for element in method.findall('*'):
-            if element.tag in data1:
-                raise ValueError("In <torenbeek> key %r already exists."% element.tag)
-            else:
+        for element in method.findall('**'):
+            if ('tunit' in element.tag) and (element.tag not in data1):
+                data1[element.tag] = element.text
+            elif ('ttype' in element.tag) and (element.tag not in data1):
+                data1[element.tag] = element.text
+            elif (('tunit' and 'ttype') not in element.tag) and (element.tag not in data1):
                 data1[element.tag] = float(element.text)
+            else:
+                raise ValueError("In <torenbeek>, key %r already exists." % element.tag)
 
     for method in fp.findall('raymer'):
-        for element in method.findall('*'):
-            if element.tag in data2:
-                raise ValueError("In <raymer> key %r already exists." % element.tag)
-            else:
+        for element in method.findall('**'):
+            if ('rtype' in element.tag) and (element.tag not in data2):
+                data2[element.tag] = element.text
+            elif ('rtype' not in element.tag) and (element.tag not in data2):
                 data2[element.tag] = float(element.text)
+            else:
+                raise ValueError("In <raymer> key, %r already exists." % element.tag)
 
     for method in fp.findall('gd'):
         for element in method.findall('*'):
