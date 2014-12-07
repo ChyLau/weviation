38ca2b8de0fecc203fbc7c641373870050c9b008
Chy Lau <chyho.lau@gmai.com>
2014-12-08 00:19:05 +0100

    It's working! Now the units and types needs to be added for Torenbeek, the other methods will be done later. Also the piechart window needs to be started soon.

--- weviation/notebook.py
+++ weviation/notebook.py
@@ -3,16 +3,19 @@ import methods
 import parse as p
 
 class TabPanel(wx.ScrolledWindow):
-    def __init__(self, parent, dlist):
+    def __init__(self, parent):
         """"""
         wx.ScrolledWindow.__init__(self, parent)
         self.SetScrollRate(5,15)
 
-        self.dlist = dlist
         self.init_tab()
 
 
     def init_tab(self):
+        self.parameters = ['marker', 'line', 'w_g','b_ref', 'Lambda', 'b', 'n_ult', 's_w', 't_r', 'marker', 'line', 's_v', 's_h', 'Lambda_h', 'Lambda_v', 'marker', 'line', 'v_d', 'l_t', 'b_f', 'h_f', 's_g', 'marker', 'line', 'p_to', 'marker', 'line',  'a_m', 'b_m', 'c_m', 'd_m', 'a_n', 'b_n', 'c_n', 'd_n', 'w_to', 'marker', 'line', 'n_e', 'w_e', 'marker', 'line', 'w_fto', 'marker', 'line', 'l_d', 'n_i', 'a_i', 'marker', 'line', 'ax', 't_to', 'marker', 'line', 'n_ft', 'v_ft', 'marker', 'line', 'v_wt', 'marker', 'line', 'n_p','b_p','d_p', 'marker', 'line', 'w_ba', 'marker', 'line', 'w_de','r_d', 'marker', 'line', 'p_el','marker', 'line', 'l_pax', 'marker', 'line', 'n_pax', 'marker','line', 'w_zf']
+
+        self.components = ['wing', 'tail', 'fuselage', 'nacelle', 'landing main', 'landing nose', 'surface controls', 'engine', 'accessory', 'air induction', 'exhaust', 'oil/cooler', 'fuel system', 'water injection', 'propeller installation', 'thrust reversers', 'APU', 'instruments', 'hydraulic/lectrical', 'AC/pressure/anti-ice', 'oxygen system', 'furnishing']
+
         hbox = wx.BoxSizer(wx.VERTICAL)
 
         stxt0 = wx.StaticText(self, label='Components')
@@ -27,7 +30,14 @@ class TabPanel(wx.ScrolledWindow):
         hbox.Add(self.cball, 0)
         self.Bind(wx.EVT_CHECKBOX, self.select_all, self.cball)
 
+        self.cb_dict = {}
         # component checkboxes
+        for item in self.components:
+            cb = wx.CheckBox(self, label=item)
+            vbox0.Add(cb, 0)
+            self.cb_dict[item] = cb
+
+        """
         self.cb_w = wx.CheckBox(self, label='wing')
         self.cb_t = wx.CheckBox(self, label='tail')
         self.cb_f = wx.CheckBox(self, label='fuselage')
@@ -50,25 +60,27 @@ class TabPanel(wx.ScrolledWindow):
         self.cb_api = wx.CheckBox(self, label='AC/pressure/anti-ice')
         self.cb_ox = wx.CheckBox(self, label='oxygen system')
         self.cb_fur = wx.CheckBox(self, label='furnishing')
-
+        """
         # component checkboxes list
-        self.cb_comp = [self.cb_w, self.cb_t, self.cb_f, self.cb_n, self.cb_ucm, self.cb_ucn, self.cb_sc, self.cb_eni, self.cb_acc, self.cb_airi, self.cb_ext, self.cb_oc, self.cb_fsi, self.cb_wis, self.cb_pi, self.cb_tr, self.cb_apu, self.cb_navp, self.cb_heu, self.cb_api, self.cb_ox, self.cb_fur]
+        #self.cb_comp = [self.cb_w, self.cb_t, self.cb_f, self.cb_n, self.cb_ucm, self.cb_ucn, self.cb_sc, self.cb_eni, self.cb_acc, self.cb_airi, self.cb_ext, self.cb_oc, self.cb_fsi, self.cb_wis, self.cb_pi, self.cb_tr, self.cb_apu, self.cb_navp, self.cb_heu, self.cb_api, self.cb_ox, self.cb_fur]
 
         # add checkboxes
+        """
         for i, cb in enumerate(self.cb_comp):
             vbox0.Add(cb, 0)
+        """
 
         hbox.Add(vbox0, 0)
 
         # component list
-        comp = ['Wing', 'Tail', 'Fuselage', 'Nacelle,..', 'Landing gear,..', 'Engine,..', 'Accessory', 'Air induction', 'Exhaust', 'Fuel system', 'Water injection', 'Propeller installation', 'APU', 'Instruments, ...', 'Electrical', 'Air conditioning', 'Oxygen system', 'Furnishing']
+        comp_title = ['Wing', 'Tail', 'Fuselage', 'Nacelle,..', 'Landing gear,..', 'Engine,..', 'Accessory', 'Air induction', 'Exhaust', 'Fuel system', 'Water injection', 'Propeller installation', 'APU', 'Instruments, ...', 'Electrical', 'Air conditioning', 'Oxygen system', 'Furnishing']
 
         sizer = wx.GridBagSizer(0, 0)
         j = 0 # 'comp' list index
         self.tc_dict = {} # dict of TextCtrl
-        for i, item in enumerate(self.dlist):
+        for i, item in enumerate(self.parameters):
             if item == 'marker':
-                comp_name = wx.StaticText(self, label=comp[j])
+                comp_name = wx.StaticText(self, label=comp_title[j])
                 font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
                 comp_name.SetFont(font)
                 j += 1
@@ -89,6 +101,7 @@ class TabPanel(wx.ScrolledWindow):
     def calculate_weight(self):
 
         d1 = {}
+
         for key, value in self.tc_dict.iteritems():
             if value.GetValue() is not u'':
                 d1[key] = float(value.GetValue())
@@ -130,10 +143,15 @@ class TabPanel(wx.ScrolledWindow):
         d1['ttype_heu']= 'utility'
         d1['ttype_ox']= 'below'
 
+        weights = ['w_w', 'w_tail', 'w_f', 'w_n', 'w_ucm', 'w_ucn', 'w_sc', 'w_eni', 'w_acc', 'w_airi', 'w_ext', 'w_oc', 'w_fsi', 'w_wis', 'w_pi', 'w_tr', 'w_apu', 'w_navp', 'w_heu', 'w_api', 'w_ox', 'w_fur']
+
+        # dictionary of weights and components
+        reference = {}
+        for i, weight in enumerate(weights):
+            reference[self.components[i]] = weight
+
         tor['w_w'] = torenbeek.w_w(d1['w_g'], d1['b_ref'], d1['Lambda'], d1['b'], d1['n_ult'], d1['s_w'], d1['t_r'], 'si')
-        tor['w_tail'] = torenbeek.w_tail(d1['n_ult'], d1['s_v'], d1['s_h'], d1['tunit_tail'])
-        tor['w_htail'] = torenbeek.w_htail(d1['s_h'], d1['v_d'], d1['Lambda_h'], d1['ttype_htail'])
-        tor['w_vtail'] = torenbeek.w_vtail(d1['s_v'], d1['v_d'], d1['Lambda_v'], d1['ttype_vtail'])
+        tor['w_tail'] = torenbeek.w_htail(d1['s_h'], d1['v_d'], d1['Lambda_h'], d1['ttype_htail']) + torenbeek.w_vtail(d1['s_v'], d1['v_d'], d1['Lambda_v'], d1['ttype_vtail'])
         tor['w_f'] = torenbeek.w_f(d1['v_d'], d1['l_t'], d1['b_f'], d1['h_f'], d1['s_g'], d1['tunit_f'], d1['ttype_f'])
         tor['w_n'] = torenbeek.w_n(d1['p_to'], d1['tunit_n'])
         tor['w_ucm'] = torenbeek.w_uc(d1['a_m'], d1['b_m'], d1['c_m'], d1['d_m'], d1['w_to'], d1['tunit_ucm'], d1['ttype_ucm'])
@@ -156,11 +174,12 @@ class TabPanel(wx.ScrolledWindow):
         tor['w_fur'] = torenbeek.w_fur(d1['w_zf'], d1['tunit_fur'])
 
         ret = ""
-        for key, value in tor.iteritems():
-            for item in self.cb_comp:
-                if item.GetValue() == True:
-                    ret += str(value) + "\n"
+        ret += "TORENBEEK" + "\n" + "------------------------------" + "\n"
+        for component in self.components:
+            if self.cb_dict[component].GetValue() == True:
+                ret += component + ":      " + str(round(tor[reference[component]], 2)) + "\n"
 
+        ret += "total:      " + str(round(sum(tor.values()), 2))
         return ret
 
     def load_xml(self, d1):
@@ -173,11 +192,11 @@ class TabPanel(wx.ScrolledWindow):
         label = evt.GetEventObject().GetValue()
 
         if label:
-            for cb in self.cb_comp:
-                cb.SetValue(True)
+            for key, value in self.cb_dict.iteritems():
+                value.SetValue(True)
         else:
-            for cb in self.cb_comp:
-                cb.SetValue(False)
+            for key, value in self.cb_dict.iteritems():
+                value.SetValue(False)
 
 
 
@@ -189,20 +208,18 @@ class DemoFrame(wx.Frame):
                           size=(1200,800)
                           )
 
-        tlist = ['marker', 'line', 'w_g','b_ref', 'Lambda', 'b', 'n_ult', 's_w', 't_r', 'marker', 'line', 's_v', 's_h', 'Lambda_h', 'Lambda_v', 'marker', 'line', 'v_d', 'l_t', 'b_f', 'h_f', 's_g', 'marker', 'line', 'p_to', 'marker', 'line',  'a_m', 'b_m', 'c_m', 'd_m', 'a_n', 'b_n', 'c_n', 'd_n', 'w_to', 'marker', 'line', 'n_e', 'w_e', 'marker', 'line', 'w_fto', 'marker', 'line', 'l_d', 'n_i', 'a_i', 'marker', 'line', 'ax', 't_to', 'marker', 'line', 'n_ft', 'v_ft', 'marker', 'line', 'v_wt', 'marker', 'line', 'n_p','b_p','d_p', 'marker', 'line', 'w_ba', 'marker', 'line', 'w_de','r_d', 'marker', 'line', 'p_el','marker', 'line', 'l_pax', 'marker', 'line', 'n_pax', 'marker','line', 'w_zf']
-
         panel = wx.Panel(self)
 
         # tabs
         notebook = wx.Notebook(panel)
 
-        self.tabOne = TabPanel(notebook, tlist)
+        self.tabOne = TabPanel(notebook)
         notebook.AddPage(self.tabOne, "Torenbeek")
 
-        self.tabTwo = TabPanel(notebook, tlist)
+        self.tabTwo = TabPanel(notebook)
         notebook.AddPage(self.tabTwo, "Raymer")
 
-        self.tabThree = TabPanel(notebook, tlist)
+        self.tabThree = TabPanel(notebook)
         notebook.AddPage(self.tabThree, "General Dynamics")
 
         # calculate button
@@ -210,9 +227,9 @@ class DemoFrame(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.calculate_button, btnCalc)
 
         # method checkbox
-        cb_tor = wx.CheckBox(panel, label='Torenbeek')
-        cb_ray = wx.CheckBox(panel, label='Raymer')
-        cb_gd = wx.CheckBox(panel, label='General Dynamics')
+        self.cb_tor = wx.CheckBox(panel, label='Torenbeek')
+        self.cb_ray = wx.CheckBox(panel, label='Raymer')
+        self.cb_gd = wx.CheckBox(panel, label='General Dynamics')
 
         # load xml button
         btnLoad = wx.Button(panel, label='Load XML', size=(80,30))
@@ -230,9 +247,9 @@ class DemoFrame(wx.Frame):
         hbox0 = wx.BoxSizer(wx.HORIZONTAL)
 
         hbox0.Add(btnCalc)
-        hbox0.Add(cb_tor)
-        hbox0.Add(cb_ray)
-        hbox0.Add(cb_gd)
+        hbox0.Add(self.cb_tor)
+        hbox0.Add(self.cb_ray)
+        hbox0.Add(self.cb_gd)
         hbox0.Add(btnLoad)
         vbox0.Add(hbox0)
 
@@ -249,7 +266,8 @@ class DemoFrame(wx.Frame):
         label = evt.GetEventObject().GetLabel()
 
         if label == 'Calculate':
-            self.txt1.SetValue(self.tabOne.calculate_weight())
+            if self.cb_tor.GetValue() == True:
+                self.txt1.SetValue(self.tabOne.calculate_weight())
 
     def load_button(self, evt):
         label = evt.GetEventObject().GetLabel()
